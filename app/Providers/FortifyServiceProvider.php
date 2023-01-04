d1545511b2a5000a3d1c42b63695616c82bfba9b
IbuknyaHoamp <sukarnis243@gmail.com>
2023-01-04 07:45:50 +0700

    setting fortify

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -41,5 +41,17 @@ class FortifyServiceProvider extends ServiceProvider
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
         });
+
+        // untuk view
+
+        // login
+        Fortify::loginView(function() {
+            return view('auth.login');
+        });
+
+        // register
+        Fortify::registerView(function(){
+            return view('auth.register');
+        });
     }
 }
